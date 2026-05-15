33428fe5cbdbb473e7fc871bce7f73e4ad5222b3
IT25100581 <it25100581@my.sliit.lk>
2026-05-15 12:10:23 +0530

    add grade class

---
+++ src/main/java/com/example/courseregistrationsystem/model/Grade.java
@@ -0,0 +1,13 @@
+package com.example.courseregistrationsystem.model;
+
+public class Grade {
+    private int id;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
