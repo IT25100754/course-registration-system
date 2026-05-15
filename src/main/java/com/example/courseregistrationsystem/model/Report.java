e0cb5ce1969292205e1bb5ac2b0f92ad867b6e15
IT25100754 <IT25100754@my.sliit.lk>
2026-05-15 13:38:58 +0530

    Add admin grade controller

---
+++ src/main/java/com/example/courseregistrationsystem/model/Report.java
@@ -0,0 +1,17 @@
+package com.example.courseregistrationsystem.model;
+
+public class Report {
+    private int studentId;
+
+    public Report(){
+
+    }
+
+    public int getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(int studentId) {
+        this.studentId = studentId;
+    }
+}
