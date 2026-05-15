b8cb22a5f2e64f7bf8d7defe0721430389356276
IT25100754 <IT25100754@my.sliit.lk>
2026-05-15 16:31:32 +0530

    delete revert last change

---
+++ src/main/java/com/example/courseregistrationsystem/model/payment.java
@@ -0,0 +1,13 @@
+package com.example.courseregistrationsystem.model;
+
+public class payment {
+    private  int paymentID;
+
+    public int getPaymentID() {
+        return paymentID;
+    }
+
+    public void setPaymentID(int paymentID) {
+        this.paymentID = paymentID;
+    }
+}
